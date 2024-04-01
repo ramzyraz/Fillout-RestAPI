7dda2f390b11572fa90ad3869d5f4249e3276bbc
ramez.salman <ramez.salman@onestream.live>
2024-04-02 00:35:06 +0500

    Added dotenv package

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const API_BASE_URL = 'https://api.fillout.com';
-const API_KEY = process.env.API_KEY || 'sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912';
+const API_KEY = process.env.API_KEY;
 
 module.exports = { API_BASE_URL, API_KEY };
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const formsRouter = require('./routes');
 
